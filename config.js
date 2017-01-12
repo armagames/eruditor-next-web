ad4ad077b004e46f4c5a03243b09d3456b42c6f8
Sergey Kochkin <reboot@armagames.org>
2017-01-12 20:44:36 +0500

    add loader indicator

--- config.js
+++ config.js
@@ -2,6 +2,8 @@ window.InfinniUI = window.InfinniUI || {};
 window.InfinniUI.config = window.InfinniUI.config || {};
 window.InfinniUI.config.lang = 'en-US';
 
+window.InfinniUI.config.useLoaderIndicator = true;
+
 window.InfinniUI.config.serverUrl = 'http://armagames.org:27099'
 window.InfinniUI.config.configName = 'Eruditor.Next';
 
