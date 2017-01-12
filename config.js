bcb103f28091bbe527177f7073acb56969c2fba4
Sergey Kochkin <reboot@armagames.org>
2017-01-12 20:21:07 +0500

    first version

---
+++ config.js
@@ -0,0 +1,9 @@
+window.InfinniUI = window.InfinniUI || {};
+window.InfinniUI.config = window.InfinniUI.config || {};
+window.InfinniUI.config.lang = 'en-US';
+
+window.InfinniUI.config.serverUrl = 'http://armagames.org:27099'
+window.InfinniUI.config.configName = 'Eruditor.Next';
+
+window.InfinniUI.config.homePage = 'views/main.json';
+window.InfinniUI.config.Routes = [];
